9ccba5e16e4e6598317d219104b093d3a3c3b129
emonShimoul <emonshimoul@gmail.com>
2023-01-08 01:56:35 +0600

    display store using useSelector

--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const BooksView = () => {
+    const books = useSelector((state) => state.booksReducer.books);
+    console.log(books);
     return (
         <div>
             <h2>List Of Books</h2>
+            <table className="w-full text-sm text-left text-red-500 dark:text-red-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books && books.map((book) => {
+                        const { id, title, author } = book;
+                        return <tr key={id}>
+                            <td>{id}</td>
+                            <td>{title}</td>
+                            <td>{author}</td>
+                            <td>
+                                <button>Edit</button>
+                                <span> </span>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 };
