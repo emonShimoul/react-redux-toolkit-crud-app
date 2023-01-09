f1d7c7411015e0b03d095778a2f76d3dc12fcac0
emonShimoul <emonshimoul@gmail.com>
2023-01-09 22:38:35 +0600

    add field in the book form

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 
 const AddBook = () => {
     const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
     return (
         <div>
             <h2>Add Books</h2>
-            <form>
-                <div>
+            <form className='mt-8'>
+                <div className='mb-4'>
                     <label htmlFor="title">Title: </label>
-                    <input className='border' type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} required />
+                    <input className='border border-black' type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} required />
+                </div>
+                <div>
+                    <label htmlFor="author">Author: </label>
+                    <input className='border border-black' type="text" name="author" value={author} onChange={e => setAuthor(e.target.value)} required />
                 </div>
             </form>
         </div>
