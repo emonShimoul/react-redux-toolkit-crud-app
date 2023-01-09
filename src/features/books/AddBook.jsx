aed74ecc5b8da2d8135c656ec97dee4e4f12cc6a
emonShimoul <emonshimoul@gmail.com>
2023-01-09 22:33:28 +0600

    add book form

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddBook = () => {
+    const [title, setTitle] = useState("");
     return (
         <div>
             <h2>Add Books</h2>
+            <form>
+                <div>
+                    <label htmlFor="title">Title: </label>
+                    <input className='border' type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} required />
+                </div>
+            </form>
         </div>
     );
 };
