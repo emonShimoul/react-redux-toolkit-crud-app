2189179463c6f3470fe6b6a745f9fa2abaf7465f
emonShimoul <emonshimoul@gmail.com>
2023-01-09 22:42:54 +0600

    added a submit button

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -3,10 +3,15 @@ import React, { useState } from 'react';
 const AddBook = () => {
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const book = { title, author };
+    }
+
     return (
         <div>
             <h2>Add Books</h2>
-            <form className='mt-8'>
+            <form onSubmit={handleSubmit} className='mt-8'>
                 <div className='mb-4'>
                     <label htmlFor="title">Title: </label>
                     <input className='border border-black' type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} required />
@@ -15,6 +20,7 @@ const AddBook = () => {
                     <label htmlFor="author">Author: </label>
                     <input className='border border-black' type="text" name="author" value={author} onChange={e => setAuthor(e.target.value)} required />
                 </div>
+                <button className='bg-black text-white py-2 px-4 mt-4' type='submit'>Add Book</button>
             </form>
         </div>
     );
